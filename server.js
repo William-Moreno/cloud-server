6a2509ebf2638c2d5b9fd985ce5268cb4a1950a1
William-Moreno <intrcptr@comcast.net>
2021-03-08 16:02:41 -0800

    added and removed node modules

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 'use strict';
 
-
+require('dotenv').config();
 const express = require('express');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.static('./public'));
 
-app.listen(PORT, () => console.log('yay the server actually started'));
+app.listen(PORT, () => console.log('Yay the server actually started'));
 
